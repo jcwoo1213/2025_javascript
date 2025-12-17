57a1850a982f150a3960dc6c1962c5edf10f9cad
jcwoo <jcwoo1213@gmail.com>
2025-12-17 23:19:45 +0900

    1217과제

---
+++ js/do1217.js
@@ -0,0 +1,6 @@
+let name=prompt('이름을 입력하세요');
+let age=prompt('나이를 입력하세요');
+let address=prompt('주소를 입력하세요');
+console.log('이름은 '+ name);
+console.log('나이는 '+ age +'살');
+console.log('주소는 ' + address);
