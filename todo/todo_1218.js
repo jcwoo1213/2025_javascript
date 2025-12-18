91e043107ccade80e23566980520d3a917ced851
jcwoo <jcwoo1213@gmail.com>
2025-12-18 22:10:23 +0900

    1218 숙제완료

--- todo/todo_1218.js
+++ todo/todo_1218.js
@@ -4,9 +4,32 @@
 //5번 실행.
 //합계, 평균,최대값 구하기 => "합계:얼마, 평균:얼마,최대값:얼마"
 //함수로 만들기 함수명:scoreInfo
-
+function scoreInfo(){
+    let max=0;
+    let sum=0;
+    for(let i=1 ;i<=5;i++){
+        let score=Math.floor(Math.random()*69)+31;
+        if(score>max){
+            max=score;
+        }
+        sum+= score;
+    }
+    console.log(`합계:${sum},평균:${sum/5},최대값:${max}`);
+}
 //실행 횟수 매개값 받기
-
+function scoreInfo2(count){
+    let max=0;
+    let sum=0;
+    for(let i=1 ;i<=count;i++){
+        let score=Math.floor(Math.random()*69)+31;
+        if(score>max){
+            max=score;
+        }
+        sum+= score;
+    }
+    console.log(`합계:${sum},평균:${sum/count},최대값:${max}`);
+}
+scoreInfo();
 //git add .
 //git commit -m ""
 // git push origin main
